a4ed8275f8ff6b077bfcf6c26d8478a2c9a9acb5
Mohammed Hossain <jamalhossain1629@gmail.com>
2021-02-02 09:17:38 -0500

    done

---
+++ C_Sharp_Classes/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Classes
+{
+    public class Car
+    {
+       
+
+        public Car( string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+
+        public Car( string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ C_Sharp_Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car carOne = new Car("Honda", "Civic", 2021);
+            Car carTwo = new Car("Honda", "Fit", 2020);
+            Car catThree = new Car("Toyota", "Corolla", 2021);
+
+            Car nameAndPrice = new Car("Honda", 23000);
+            Car nP = new Car("Toyota", 25000);
+
+            var carList = new List<Car>() { carOne, carTwo, catThree, nameAndPrice, nP };
+
+            foreach( var carListOne in carList)
+            {
+                Console.WriteLine($" {carListOne.Make} {carListOne.Model} {carListOne.Year} {carListOne.Name} {carListOne.Price} ");
+            }
+
+            
+
+        }
+    }
+}
